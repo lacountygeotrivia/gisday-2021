b1cfbf50433d1f9ad124ebd43a93c2152d8305c8
lacountygeotrivia <115057354+lacountygeotrivia@users.noreply.github.com>
2022-10-14 03:12:09 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -76,6 +76,154 @@
 			}).addTo(_map);
 		}
 		
+		var url = "https://lacountygeotrivia.github.io/gisday-2021/resources/data.csv";
+		Papa.parse(
+			url, 
+			{
+				header: true,
+				download: true,
+				complete: function(data) {
+					_records = $.map(
+						data.url, 
+						function(value, index){return new Record(value, index);}
+					);
+					finish();
+				}
+			}
+		);
+
+		function finish()
+		{
+			next();
+			processPosition();
+			$("#prompt > button").click(
+				function(){$("html body").addClass("hint");_map.invalidateSize();}
+			);
+			$("button#button-next").click(function(){next();});
+			$("html body").keydown(onKeyDown);
+			$("#intro button").click(function(){$("#intro").hide();});
+			
+			/* 	hooks so that custom finish-up code can execute (minimally 
+				to add the basemap)	*/
+
+			window._map = _map;
+			if (window.onFinalFinish) {
+				window.onFinalFinish();		
+			}
+			
+		}		
+
+
+	});
+
+	/***************************************************************************
+	******************************** EVENTS ************************************
+	***************************************************************************/
+
+	function onKeyDown(event) {
+		/* next on ctrl-alt-n */
+		if (event.ctrlKey && event.altKey && event.which === 78 && _records.length) 
+		{
+			next();
+		}
+	}
+
+	function onMoveEnd()
+	{
+		if (!$("html body").hasClass("reveal")) {
+			processPosition();
+		}
+	}
+
+	/***************************************************************************
+	******************************** FUNCTIONS *********************************
+	***************************************************************************/
+	function next()
+	{
+		if (_records.length)
+		{
+			if (_selected) {
+				_prior.push(_selected);
+				_map.flyTo(_selected.getLatLng(), HOME_LEVEL);
+			}
+			_selected = _records.shift();
+			reset();
+		} else {
+			// we have reached the end.
+		}
+	}
+	
+	function reset()
+	{
+		$("#prompt").removeClass("fadeIn");		
+		$("#photo-credit").html(_selected.getImageAttribution());
+		$("#prompt > div:nth-of-type(1)").html(_selected.getText());
+		$("#hint").html(_selected.getHint());
+		$("#exclamation > div:nth-of-type(1)").html(_selected.getExclamation());
+		$("#portrait").css("background-image", "url('"+_selected.getImageURL()+"')");
+		$("#message-panel").css("background-image", "url('"+_selected.getImageURL()+"')");
+		$("html body").removeClass("reveal hint");
+		if(_marker){_marker.remove();}
+		_map.invalidateSize();
+		processPosition();
+		$("#prompt").addClass("fadeIn");
+	}
+	
+	function processPosition()
+	{
+				
+		var center = _map.getCenter();
+		var distance = center.distanceTo(_selected.getLatLng());
 		
+		var green = parseInt(255*(1-distance/SCALE_DENOMINATOR));
+		var red = parseInt(255*(distance/SCALE_DENOMINATOR));
+		$("#square").css("background-color", "rgba("+red+","+green+",0,0.6)");
+		
+		if (boxToExtent().contains(_selected.getLatLng())) {
+			if (_map.getZoom() >= _selected.getZoom()) {
+				_marker = L.marker(_selected.getLatLng())
+					.addTo(_map)
+					.bindPopup(_selected.getTitle(),{closeButton: false})
+					.openPopup();
+				$("#square").removeClass("blinking");
+				$("html body").addClass("reveal");
+				if (_records.length) {
+					$("#exclamation button").show();
+					$("#exclamation button").focus();
+					$("#exclamation a#certificate").hide();
+				} else {
+					$("#exclamation button").hide();
+					$("#exclamation a#certificate").show();
+					$("#exclamation a#certificate").focus();					
+				}
+			} else {
+				$("#square").addClass("blinking");
+				setTimeout(
+					function(){
+						_map.flyTo(_selected.getLatLng(), _selected.getZoom());
+					},
+					1000
+				);
+			}
+		}
+		
+	}
+	
+	function boxToExtent()
+	{
+		var ll1 = _map.containerPointToLatLng(
+			[
+				$("#square").position().left-$("#square").width()/2, 
+				$("#square").position().top-$("#square").height()/2
+			]
+		);
+		var ll2 = _map.containerPointToLatLng(
+			[
+				$("#square").position().left+$("#square").width()/2, 
+				$("#square").position().top+$("#square").height()/2
+			]
+		);
+		return L.latLngBounds(ll1,ll2);
+	}
 
 })();
